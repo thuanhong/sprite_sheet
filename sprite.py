c1a6ed947447c4ab305a1fb66bf0239bceb07fce
hthuan <you@example.com>
2019-09-20 20:05:20 +0700

    add

---
+++ sprite.py
@@ -0,0 +1,28 @@
+from PIL import Image
+
+
+class Sprite():
+    def __init__(self, label, x1, y1, x2, y2):
+        if any(not isinstance(x, int) or x < 0 for x in [label, x1, x2, y1, y2]) or (x2, y2) <= (x1, y1):
+            raise ValueError('Invalid coordinates')
+        self._label = label
+        self._top_left = (x1, y1)
+        self._bottom_right = (x2, y2)
+        self._width = x2 - x1 + 1
+        self._height = y2 - y1 + 1
+
+    @property
+    def label(self):
+        return self._label
+    @property
+    def top_left(self):
+        return self._top_left
+    @property
+    def bottom_right(self):
+        return self._bottom_right
+    @property
+    def width(self):
+        return self._width
+    @property
+    def height(self):
+        return self._height
---
+++ spriteutil.py
@@ -0,0 +1,21 @@
+from PIL import Image
+import timeit
+
+
+def find_most_common_color(image):
+    """
+    Get the pixel color that is the most used in this image
+    @param image : a Image object
+    @return : most common color in image
+    """
+    image = image.resize((150, 150),resample = 0) # only resize if want increase performance
+    # Get colors from image object
+    pixels = image.getcolors(image.width * image.height)
+    # Get the most frequent color
+    most_common_color = max(pixels, key=lambda t: t[0])
+    return most_common_color[1]
+
+
+image = Image.open('1.jpg')
+print(timeit.timeit(stmt=lambda: find_most_common_color(image), number=1))
+print(find_most_common_color(image))
