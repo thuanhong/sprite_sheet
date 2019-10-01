a06b8ee815328f9b0f1dac7884dc2a2b424c20da
hthuan <you@example.com>
2019-10-01 19:42:47 +0700

    update wp3

--- temp.py
+++ temp.py
@@ -1,39 +1,38 @@
 from PIL import Image
 import numpy as np
-from pprint import pprint
-from json import dumps
+from sprite import Sprite
 
 
 im = Image.open('im.png')
-array = np.asarray(im)
-rows, cols = array.shape[:2]
-new_array = np.zeros((rows, cols), dtype=np.int64)
+image_map = np.asarray(im)
+rows, cols = image_map.shape[:2]
+label_map = np.zeros((rows, cols), dtype=np.int64)
 
 label = 1
 connect = {}
 for x in range(rows):
     for y in range(cols):
-        if np.array_equal(array[x][y], [0, 0, 0, 0]):
+        if np.array_equal(image_map[x][y], [0, 0, 0, 0]):
             continue
         for i, j in [[x, y-1], [x-1, y-1], [x-1, y], [x-1, y]]:
             if (i, j) < (0, 0) or (i, j) > (rows, cols):
                 continue
-            if new_array[i][j] != 0:
-                if new_array[x][y] == 0:
-                    new_array[x][y] = new_array[i][j]
-                elif new_array[x][y] != new_array[i][j]:
-                    connect[new_array[i][j]].append(new_array[x][y])
+            if label_map[i][j] != 0:
+                if label_map[x][y] == 0:
+                    label_map[x][y] = label_map[i][j]
+                elif label_map[x][y] != label_map[i][j]:
+                    connect[label_map[i][j]].append(label_map[x][y])
                     break
         else:
-            if not new_array[x][y]:
-                new_array[x][y] = label
+            if not label_map[x][y]:
+                label_map[x][y] = label
                 connect[label] = []
                 label += 1
 
-temp_list = list(connect.keys())
+list_key_connect = list(connect.keys())
 
 
-for x in reversed(temp_list):
+for x in reversed(list_key_connect):
     count = 0
     try:
         connect[x] = list(set(connect[x]))
@@ -45,11 +44,8 @@ for x in reversed(temp_list):
         continue
 
 
-temp_list = list(connect.keys())
-
-
-for index1, key in enumerate(temp_list[:-1]):
-    for index2, key2 in enumerate(temp_list[index1+1:], index1+1):
+for index1, key in enumerate(list_key_connect[:-1]):
+    for index2, key2 in enumerate(list_key_connect[index1+1:], index1+1):
         new_list = connect[key2] + [key2]
         if any(x in connect[key] for x in new_list):
             connect[key] += new_list
@@ -58,17 +54,95 @@ for index1, key in enumerate(temp_list[:-1]):
 
 connect = {k: v for k, v in connect.items() if v}
 
-# f = open('obj.js', 'w')
-# f.write(str(connect))
-# f.close()
+sprites = {}
 
 for label, x in enumerate(connect, 1):
     for a in connect[x]:
-        new_array[new_array == a] = label
-    new_array[new_array == x]
-temp_array = np.argwhere(new_array == 1)
-x_max, y_max = np.amax(temp_array, axis=0)
-x_min, y_min = np.amin(temp_array, axis=0)
+        label_map[label_map == a] = label
+    label_map[label_map == x] = label
+
+    temp_array = np.argwhere(label_map == label)
+    x_max, y_max = np.amax(temp_array, axis=0)
+    x_min, y_min = np.amin(temp_array, axis=0)
+    sprites[label] = Sprite(label, x_min.item(), y_min.item(), x_max.item(), y_max.item())
+    # im.crop((y_min, x_min, y_max, x_max)).show()
 
 
-# np.savetxt("filename", new_array, newline="\n", fmt='%d')
+# np.savetxt("filename", label_map, newline="\n", fmt='%d')
+
+def image_segmentation(rows, cols, image_map, label_map, connect):
+    label = 1
+    for x in range(rows):
+        for y in range(cols):
+            if np.array_equal(image_map[x][y], [0, 0, 0, 0]):
+                continue
+            for i, j in [[x, y-1], [x-1, y-1], [x-1, y], [x-1, y]]:
+                if (i, j) < (0, 0) or (i, j) > (rows, cols):
+                    continue
+                if label_map[i][j] != 0:
+                    if label_map[x][y] == 0:
+                        label_map[x][y] = label_map[i][j]
+                    elif label_map[x][y] != label_map[i][j]:
+                        connect[label_map[i][j]].append(label_map[x][y])
+                        break
+            else:
+                if not label_map[x][y]:
+                    label_map[x][y] = label
+                    connect[label] = []
+                    label += 1
+    return True
+
+
+def filter_segmentation(connect, list_key_connect):
+    for x in reversed(list_key_connect):
+        count = 0
+        try:
+            connect[x] = list(set(connect[x]))
+            while True:
+                connect[x] += connect[connect[x][count]]
+                connect[x] = list(set(connect[x]))
+                count += 1
+        except (IndexError):
+            continue
+    return True
+
+
+def combine_segmentation(connect, list_key_connect):
+    for index1, key in enumerate(list_key_connect[:-1]):
+        for _, key2 in enumerate(list_key_connect[index1+1:], index1+1):
+            new_list = connect[key2] + [key2]
+            if any(x in connect[key] for x in new_list):
+                connect[key] += new_list
+                connect[key] = list(set(connect[key]))
+                connect[key2].clear()
+
+    connect = {k: v for k, v in connect.items() if v}
+    return True
+
+
+def find_sprites(image, background=None):
+    image_map = np.asarray(im)
+    rows, cols = image_map.shape[:2]
+    label_map = np.zeros((rows, cols), dtype=np.int64)
+    connect = {}
+
+    image_segmentation(rows, cols, image_map, label_map, connect)
+    list_key_connect = list(connect.keys())
+
+    filter_segmentation(connect, list_key_connect)
+    combine_segmentation(connect, list_key_connect)
+
+    sprites = {}
+    sprites = {}
+
+    for label, x in enumerate(connect, 1):
+        for a in connect[x]:
+            label_map[label_map == a] = label
+        label_map[label_map == x] = label
+
+        temp_array = np.argwhere(label_map == label)
+        x_max, y_max = np.amax(temp_array, axis=0)
+        x_min, y_min = np.amin(temp_array, axis=0)
+        sprites[label] = Sprite(label, x_min.item(), y_min.item(), x_max.item(), y_max.item())
+
+    return sprites, label_map
